c5684b6506f9a1cc3c8bf68caaab759e8fe1e803
renjie3 <thurenjie@foxmail.com>
2022-12-25 20:20:55 -0500

    grandriver_job.sh

---
+++ grandriver_job.sh
@@ -0,0 +1,27 @@
+JOB_ID=`cat job_id.log`
+echo $JOB_ID
+NEXT_JOB_ID=`expr $JOB_ID + 1`
+echo $NEXT_JOB_ID > job_id.log
+
+GPU_ID='4'
+MY_CMD="python main.py --config cifar10.yml --doc cifar10"
+MY_ROOT_PATH=`pwd`
+
+echo "cd ${MY_ROOT_PATH}" > ./cmd/cmd_${JOB_ID}.sh
+echo "MY_CMD=\"${MY_CMD} --job_id $JOB_ID \"" >> ./cmd/cmd_${JOB_ID}.sh
+echo "CUDA_VISIBLE_DEVICES='${GPU_ID}' \${MY_CMD}" >> ./cmd/cmd_${JOB_ID}.sh
+echo "\nif [ \$? -eq 0 ];then" >> ./cmd/cmd_${JOB_ID}.sh
+echo "echo -e \"grandriver JobID:${JOB_ID} \\\n Python_command: \\\n ${MY_CMD} \\\n \" | mail -s \"[Done] grandriver ${SLURM_JOB_ID}\" renjie2179@outlook.com" >> ./cmd/cmd_${JOB_ID}.sh
+echo "else" >> ./cmd/cmd_${JOB_ID}.sh
+echo "echo -e \"grandriver JobID:${JOB_ID} \\\n Python_command: \\\n ${MY_CMD} \\\n \" | mail -s \"[Fail] grandriver ${SLURM_JOB_ID}\" renjie2179@outlook.com" >> ./cmd/cmd_${JOB_ID}.sh
+echo "fi" >> ./cmd/cmd_${JOB_ID}.sh
+
+nohup sh ./cmd/cmd_${JOB_ID}.sh >./logfile/${JOB_ID}.log 2>./logfile/${JOB_ID}.err &
+
+echo $MY_CMD
+
+date >>./history_job.log
+echo ${JOB_ID}>>./history_job.log
+echo "GPU_ID=${GPU_ID}">>./history_job.log
+echo ${MY_CMD}>>./history_job.log
+echo "---------------------------------------------------------------" >>./history_job.log
